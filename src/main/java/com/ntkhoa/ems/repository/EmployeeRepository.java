f292d337ec0a5cf0ac8d4969bec89851a0c70f2d
Tankhoa15 <tankhoa1503kh@gmail.com>
2024-05-15 16:03:03 +0700

    entity - repository

---
+++ src/main/java/com/ntkhoa/ems/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.ntkhoa.ems.repository;
+
+import com.ntkhoa.ems.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+}
