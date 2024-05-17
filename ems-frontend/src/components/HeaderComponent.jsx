7a58e7395816c175cc58403bb72cf56ac94328c5
Tankhoa15 <tankhoa1503kh@gmail.com>
2024-05-18 00:28:04 +0700

    config routing

--- ems-frontend/src/components/HeaderComponent.jsx
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -4,7 +4,7 @@ const HeaderComponent = () => {
   return (
     <div>
         <header>
-            <nav className='navbar navbar-light" style="background-color: #e3f2fd;'>
+            <nav className='navbar navbar-dark bg-dark'>
                 <a className="navbar-brand">Employee Managerment System</a>
             </nav>
         </header>
